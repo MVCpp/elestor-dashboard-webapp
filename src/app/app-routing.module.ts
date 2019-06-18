1367378635f83477330c4caa67917781f5483cf0
System Administrator <root@Miguels-MacBook-Pro.local>
2019-06-17 22:37:35 -0500

    Commit - initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NegociosComponent } from './negocios/negocios.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { CuponesComponent } from './cupones/cupones.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'negocios', component:NegociosComponent},
+  {path: 'pedidos', component:PedidosComponent},
+  {path: 'cupones', component:CuponesComponent},
+  {path: 'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
